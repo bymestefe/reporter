483313e827f66a65e01425d9fd55bcd7f9cf5b4e
mrsahbaz <mustafa.sahbaz@siemplus.com>
2024-07-19 14:31:38 +0300

    added new feature to generate pdf report

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const QueueDatabase = require('./query_handlers/postgres');
 const Helpers = require('./helpers');
 
+const PDFReportGenerator = require('./report_generator');
+
 const main = async () => {
   const queueDb = new QueueDatabase();
   await queueDb.createTableIfNotExists();
-  Helpers.runInterval(queueDb.checkNewRows);
+  const PdfGenerator = new PDFReportGenerator();
+  Helpers.runInterval(queueDb.checkNewRows, PdfGenerator);
 };
 
 main();
